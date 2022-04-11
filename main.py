71711c897a97ba3eb95b151782411880ed3e25c8
Winnie-Waiguru <winniewaiguru@gmail.com>
2022-04-11 18:51:27 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+height= float(input("Enter heigh in inches: "))
+weight= float(input("Enter weight in lbs: "))
+
+def BMI(height, weight):
+    bmi= weight/(height**2) *703
+    
+    if (bmi < 16):
+        return "Severly underweight", bmi
+    elif(bmi >=16 and bmi<18.5):
+        return "Underweight", bmi
+    elif(bmi >=18.5 and bmi<25):
+        return "Healthy", bmi
+    elif(bmi >=25 and bmi<30):
+        return "Overweight", bmi
+    elif(bmi >=30):
+        return "Obese", bmi
+
+quote, bmi = BMI(height, weight) 
+print("Your bmi is: {} and you are {}".format(bmi, quote))  
